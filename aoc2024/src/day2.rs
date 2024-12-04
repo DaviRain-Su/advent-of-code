f67ac67f07b1ff087e449d411df43c9bca3677b3
Davirain <davirain.yin@gmail.com>
2024-12-04 08:41:22 +0800

    add day2

---
+++ aoc2024/src/day2.rs
@@ -0,0 +1,69 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+pub struct Day2 {}
+
+impl Day2 {
+    pub fn run(&self) -> anyhow::Result<()> {
+        let input = read_input()?;
+        let answer = self.solve(input);
+        println!("Answer: {}", answer);
+        Ok(())
+    }
+
+    pub fn solve(&self, input: Vec<Vec<usize>>) -> usize {
+        input
+            .into_iter()
+            .filter(|v| {
+                // 检查序列是否单调递增或单调递减
+                let increasing = v.windows(2).all(|w| w[1] > w[0]);
+                let decreasing = v.windows(2).all(|w| w[1] < w[0]);
+
+                // 检查相邻数字之间的差值是否在1到3之间
+                let valid_diff = v.windows(2).all(|w| {
+                    let diff = if w[0] > w[1] {
+                        w[0] - w[1]
+                    } else {
+                        w[1] - w[0]
+                    };
+                    diff >= 1 && diff <= 3
+                });
+
+                (increasing || decreasing) && valid_diff
+            })
+            .count()
+    }
+}
+
+fn read_input() -> anyhow::Result<Vec<Vec<usize>>> {
+    let input = include_str!("../../data/day2.txt");
+    // parse string separated by \n and parse each line to usize by whitespace
+    // return a tuple of (usize, usize)
+    let split = input.split("\n").collect::<Vec<&str>>();
+    let split = split
+        .iter()
+        .map(|s| s.split_whitespace())
+        .map(|s| s.collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
+    let split = split
+        .iter()
+        .map(|s| {
+            s.iter()
+                .map(|s| s.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .filter(|s| !s.is_empty())
+        .collect::<Vec<Vec<usize>>>();
+
+    Ok(split)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_input2() {
+        let _input = read_input().unwrap();
+    }
+}
--- aoc2024/src/main.rs
+++ aoc2024/src/main.rs
@@ -1,16 +1,20 @@
 use clap::Parser;
 
 pub mod day1;
+pub mod day2;
 
 #[derive(Parser, Debug)]
 pub enum Args {
     #[clap(name = "day1")]
     Day1(day1::Day1),
+    #[clap(name = "day2")]
+    Day2(day2::Day2),
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     match args {
         Args::Day1(day1) => day1.run(),
+        Args::Day2(day2) => day2.run(),
     }
 }
